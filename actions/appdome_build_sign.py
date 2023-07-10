0d77eba9bfd5a2b142f14c9753d14398974bd428
Eldan Vaknin <eldan@appdome.com>
2023-07-10 17:24:28 +0300

    added second output to the yml file, got it in appdome_build_sign.py and added to the relevant cmd commands (NF-53742)

--- actions/appdome_build_sign.py
+++ actions/appdome_build_sign.py
@@ -31,6 +31,8 @@ def parse_args():
                         help="signing_fingerprint", default="None")
     parser.add_argument("-bl", dest='build_with_logs', required=True,
                         help="Do you want to build with logs?")
+    parser.add_argument("--sign_second_output", dest='sign_second_output', required=False,
+                        help="Universal apk output for aab apps?")
     return parser.parse_args()
 
 
@@ -60,6 +62,8 @@ def main():
     app_ext = app_name[-4:]
     keystore_file = glob.glob('./files/cert.*')
     build_with_logs = " -bl" if args.build_with_logs != "false" else ""
+    sign_second_output = " --sign_second_output ./output/Appdome_secured_app.apk" if (args.sign_second_output and app_ext == ".aab") \
+        else ""
     team_id = f"--team_id {args.team_id}" if args.team_id != "None" else ""
     provision_profiles = f"--provisioning_profiles {' '.join(glob.glob('./files/provision_profiles/*'))}" \
         if os.path.exists("./files/provision_profiles") else ""
@@ -73,7 +77,7 @@ def main():
               f"--sign_on_appdome -fs {fusion_set} {team_id} --keystore {keystore_file[0]} " \
               f"--keystore_pass {keystore_pass} --output ./output/Appdome_secured_app{app_ext} " \
               f"--certificate_output ./output/certificate.pdf {keystore_alias} {keystore_key_pass} " \
-              f"{provision_profiles} {entitlements}{build_with_logs}"
+              f"{provision_profiles} {entitlements}{build_with_logs}{sign_second_output}"
 
         subprocess.check_output([i for i in cmd.split(" ") if i != ''], env=new_env)
 
@@ -84,7 +88,7 @@ def main():
         cmd = f"python3 appdome/appdome-api-python/appdome_api.py -key {appdome_api_key} " \
               f"--app {app_file} --private_signing -fs {fusion_set} {team_id} " \
               f"--output ./output/Appdome_secured_app{app_ext} --certificate_output ./output/certificate.pdf " \
-              f"{google_play_signing} {signing_fingerprint} {provision_profiles}{build_with_logs}"
+              f"{google_play_signing} {signing_fingerprint} {provision_profiles}{build_with_logs}{sign_second_output}"
 
         subprocess.check_output([i for i in cmd.split(" ") if i != ''], env=new_env)
 
@@ -95,7 +99,8 @@ def main():
         cmd = f"python3 appdome/appdome-api-python/appdome_api.py -key {appdome_api_key} " \
               f"--app {app_file} --auto_dev_private_signing -fs {fusion_set} {team_id} " \
               f"--output ./output/Appdome_secured_app{app_ext} --certificate_output ./output/certificate.pdf " \
-              f"{google_play_signing} {signing_fingerprint} {provision_profiles} {entitlements}{build_with_logs}"
+              f"{google_play_signing} {signing_fingerprint} {provision_profiles} {entitlements}{build_with_logs}" \
+              f"{sign_second_output}"
         subprocess.check_output([i for i in cmd.split(" ") if i != ''], env=new_env)
     else:
         print("Signing option not found!\nValid signs: AUTO_SIGNING/PRIVATE_SIGNING/AUTO_DEV_SIGNING")
