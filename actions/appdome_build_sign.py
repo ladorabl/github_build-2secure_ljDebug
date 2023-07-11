d79dfe0dd2a70c78757258c1b9998985e460d893
Eldan Vaknin <eldan@appdome.com>
2023-07-11 13:42:11 +0300

    changed second output name to fix the problem of finding 2 files in the run

--- actions/appdome_build_sign.py
+++ actions/appdome_build_sign.py
@@ -64,7 +64,7 @@ def main():
     app_ext = app_name[-4:]
     keystore_file = glob.glob('./files/cert.*')
     build_with_logs = " -bl" if args.build_with_logs != "false" else ""
-    sign_second_output = " --sign_second_output ./output/Appdome_secured_app.apk" if \
+    sign_second_output = " --sign_second_output ./output/Appdome_secured_app_second_output.apk" if \
         (args.sign_second_output and app_ext == ".aab") else ""
     build_to_test = f" -bt AUTOMATION_{args.build_to_test}" if args.build_to_test else ""
     team_id = f"--team_id {args.team_id}" if args.team_id != "None" else ""
