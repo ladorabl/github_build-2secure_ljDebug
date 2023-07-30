65f1a5a8485be78af934e776aae51adcb4183eef
Eldan Vaknin <eldan@appdome.com>
2023-07-30 14:31:18 +0300

    remove SERVER_BASE_URL env

--- actions/appdome_build_sign.py
+++ actions/appdome_build_sign.py
@@ -42,7 +42,6 @@ sys.path.extend([os.path.join(sys.path[0], '../..')])
 new_env = os.environ.copy()
 
 new_env["APPDOME_CLIENT_HEADER"] = "Github/1.0.0"
-new_env["APPDOME_SERVER_BASE_URL"] = "https://qamaster.dev.appdome.com/"
 
 args = parse_args()
 
