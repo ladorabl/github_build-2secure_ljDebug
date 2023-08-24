4092a5795b5736bb3d0f28926518f254aa12802a
eldanv129 <50116149+eldanv129@users.noreply.github.com>
2023-08-24 13:23:50 +0300

    fix required keystore in files_manager

--- actions/files_manager.py
+++ actions/files_manager.py
@@ -61,7 +61,6 @@ def main():
     keystore = args.keystore
     if keystore == "None":
         print("No keystore file specified")
-        exit(1)
     provision_profiles = args.provision_profiles
     entitlements = args.entitlements
 
