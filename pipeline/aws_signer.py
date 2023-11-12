cb4aff2717cf66d9a880b321e0055b916169d8b1
Eldan Vaknin <eldan@appdome.com>
2023-11-12 16:59:11 +0200

    s3 paths changed

--- pipeline/aws_signer.py
+++ pipeline/aws_signer.py
@@ -8,20 +8,20 @@ def main():
     aws_default_region = 'eu-central-1'
     bucket_name = "appdome-automation-vanilla-apps"
     objects = {
-        'aab_app': 'Thomas/FileFinder.aab',
-        'apk_app': 'Thomas/TimeCard.apk',
-        'keystore_file': 'Thomas/appdome.keystore',
-        'ipa_app_1': 'Thomas/FileFinder.ipa',
-        'ipa_app_2': 'Thomas/Trends256-iOS16.ipa',
-        'certificate_file': 'Thomas/AutomationCert.p12',
-        'ipa_1_mobile_provisioning': 'Thomas/Automation.mobileprovision',
-        'ipa_1_entitlements': 'Thomas/AutomationEntitlements.plist',
-        'ipa_2_mobile_provisioning_1': 'Thomas/Trendsappstoredist.mobileprovision',
-        'ipa_2_mobile_provisioning_2': 'Thomas/Trends_watchkit_appstoredist.mobileprovision',
-        'ipa_2_mobile_provisioning_3': 'Thomas/Trends_watchkit_extension_appstoredist.mobileprovision',
-        'ipa_2_entitlements_1': 'Thomas/main.plist',
-        'ipa_2_entitlements_2': 'Thomas/watchkit.plist',
-        'ipa_2_entitlements_3': 'Thomas/watchkitextension.plist',
+        'aab_app': 'Thomas/PipelineFiles/Apps/FileFinder.aab',
+        'apk_app': 'Thomas/PipelineFiles/Apps/TimeCard.apk',
+        'keystore_file': 'Thomas/PipelineFiles/appdome.keystore',
+        'ipa_app_1': 'Thomas/PipelineFiles/Apps/FileFinder.ipa',
+        'ipa_app_2': 'Thomas/PipelineFiles/Apps/Trends256-iOS16.ipa',
+        'certificate_file': 'Thomas/PipelineFiles/AutomationCert.p12',
+        'ipa_1_mobile_provisioning': 'Thomas/PipelineFiles/Automation.mobileprovision',
+        'ipa_1_entitlements': 'Thomas/PipelineFiles/AutomationEntitlements.plist',
+        'ipa_2_mobile_provisioning_1': 'Thomas/PipelineFiles/Trendsappstoredist.mobileprovision',
+        'ipa_2_mobile_provisioning_2': 'Thomas/PipelineFiles/Trends_watchkit_appstoredist.mobileprovision',
+        'ipa_2_mobile_provisioning_3': 'Thomas/PipelineFiles/Trends_watchkit_extension_appstoredist.mobileprovision',
+        'ipa_2_entitlements_1': 'Thomas/PipelineFiles/main.plist',
+        'ipa_2_entitlements_2': 'Thomas/PipelineFiles/watchkit.plist',
+        'ipa_2_entitlements_3': 'Thomas/PipelineFiles/watchkitextension.plist',
     }
 
     if aws_access_key_id is None or aws_secret_access_key is None:
